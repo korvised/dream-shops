5df723507721a5fe8418b28bc3b0aff0d4a5c8df
Korvised YOTSAIY <kvsyotsaiy@gmail.com>
2024-10-29 10:45:25 +0700

    add model

--- src/main/java/com/korvised/dreamshops/medel/Image.java
+++ src/main/java/com/korvised/dreamshops/medel/Image.java
@@ -1,4 +1,33 @@
 package com.korvised.dreamshops.medel;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileName;
+
+    private String fileType;
+
+    @Lob
+    private Blob image;
+
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
 }
